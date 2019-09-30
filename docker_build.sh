3c1fef35a307c0ac6276d1aa5d99a717b8957277
Axel <axel.herrera@kronops.com.mx>
2019-09-30 16:19:29 -0500

    se corrigen bash

--- docker_build.sh
+++ docker_build.sh
@@ -2,59 +2,63 @@
 
 source $HOME/allvars
 POPPINS_APP_PORT=8081:8081
+export ENVIRONMENT="stg"
 
 build() {
-	echo -n ""
-	echo -n "Building ${POPPINS_DOCKER_REPO} image."
-	echo -n ""
+	echo -e ""
+	echo -e "Building ${POPPINS_DOCKER_REPO} image."
+	echo -e ""
 	docker build -t ${POPPINS_DOCKER_REPO} .
-	echo -n "Listing ${POPPINS_DOCKER_REPO} image."
+	echo -e "Listing ${POPPINS_DOCKER_REPO} image."
 	docker images
 }
 
 test () {
-	echo -n "Run ${POPPINS_DOCKER_REPO} image."
-	docker run --name ${POPPINS_APP_NAME} -p ${POPPINS_APP_PORT} -d ${POPPINS_DOCKER_REPO}
-	echo -n "Wait until NiFi is fully started."
+	echo -e "Run ${POPPINS_DOCKER_REPO} image."
+	docker run --eame ${POPPINS_APP_NAME} -p ${POPPINS_APP_PORT} -d ${POPPINS_DOCKER_REPO}
+	echo -e "Wait until NiFi is fully started."
 	sleep 15
 	docker logs ${POPPINS_APP_NAME}
-	echo -n "Connect to nifi using nifi-toolkit."
+	echo -e "Connect to nifi using nifi-toolkit."
 	#docker exec ${POPPINS_APP_NAME} /opt/nifi/nifi-toolkit-current/bin/cli.sh nifi current-user 2>/dev/null; true
 }
 
 release() {
-	echo -n "Push ${POPPINS_DOCKER_REPO} image to docker registry."
+	echo -e "Push ${POPPINS_DOCKER_REPO} image to docker registry."
 	cat ${DOCKER_PWD} | docker login --username ${DOCKER_USER} --password-stdin
 	docker tag  ${POPPINS_DOCKER_REPO} ${POPPINS_DOCKER_REPO}
 	docker push ${POPPINS_DOCKER_REPO}
 }
 
 clean() {
-	echo -n ""
-	echo -n "Cleaning local build environment."
-	echo -n ""
+	echo -e ""
+	echo -e "Cleaning local build environment."
+	echo -e ""
 	docker stop ${POPPINS_APP_NAME} 2>/dev/null; true
 	docker rm ${POPPINS_APP_NAME}  2>/dev/null; true
-	echo -n ""
-	echo -n "Purging local images."
+	echo -e ""
+	echo -e "Purging local images."
 	docker rmi ${POPPINS_DOCKER_REPO} 2>/dev/null; true
 }
 
 help() {
-	echo -n ""
-	echo -n "Please use \`make <target>' where <target> is one of"
-	echo -n ""
-	echo -n "  build		Builds the docker image."
-	echo -n "  test		Tests image."
-	echo -n "  release	Releases images."
-	echo -n "  clean		Cleans local images."
-	echo -n ""
-	echo -n ""
+	echo -e ""
+	echo -e "Please use \`make <target>' where <target> is one of"
+	echo -e ""
+	echo -e "  build		Builds the docker image."
+	echo -e "  test		Tests image."
+	echo -e "  release	Releases images."
+	echo -e "  clean		Cleans local images."
+	echo -e ""
+	echo -e ""
 }
 
-[[ "$1" == "build" ]] && build
-[[ "$1" == "test" ]] && test
-[[ "$1" == "release" ]] && release
-[[ "$1" == "clean" ]] && clean
-[[ "$1" == "help" ]] && help
-[[ "$1" == "" ]] && exit -1
+if [[ "$1" == "build" ]]; then build;
+elif [[ "$1" == "test" ]]; then test;
+elif [[ "$1" == "release" ]];then release;
+elif [[ "$1" == "clean" ]]; then clean;
+elif [[ "$1" == "help" ]];then help;
+else
+	help
+	exit -1
+fi
--- registry/docker_build.sh
+++ registry/docker_build.sh
@@ -1,58 +1,61 @@
 #!/bin/bash
 
 source $HOME/allvars
-export $ENVIRONMENT="stg"
+export ENVIRONMENT="stg"
 APP_PORT=18080:18080
 
 build() {
-	echo -n ""
-	echo -n "Building $REGISTRY_DOCKER_REPO image."
+	echo -e ""
+	echo -e "Building $REGISTRY_DOCKER_REPO image."
 	docker build -t $REGISTRY_DOCKER_REPO .
-	echo -n "Listing $REGISTRY_DOCKER_REPO image."
+	echo -e "Listing $REGISTRY_DOCKER_REPO image."
 	docker images
 }
 
 test() {
-	echo -n "Run ${REGISTRY_DOCKER_REPO} image."
+	echo -e "Run ${REGISTRY_DOCKER_REPO} image."
 	docker run --name ${REGISTRY_APP_NAME} -p ${APP_PORT} -d ${REGISTRY_DOCKER_REPO}
-	echo -n "Wait until NiFi is fully started."
+	echo -e "Wait until NiFi is fully started."
 	sleep 15
 	docker logs ${REGISTRY_APP_NAME}
 }
 
 release() {
-	echo -n "Push ${REGISTRY_DOCKER_REPO} image to docker registry."
+	echo -e "Push ${REGISTRY_DOCKER_REPO} image to docker registry."
 	#cat ${DOCKER_PWD} | docker login --username ${DOCKER_USER} --password-stdin
 	docker tag  ${REGISTRY_DOCKER_REPO} ${REGISTRY_DOCKER_REPO}
 docker push ${REGISTRY_DOCKER_REPO}
 }
 
 clean() {
-	echo -n ""
-	echo -n "Cleaning local build environment."
-	echo -n ""
+	echo -e ""
+	echo -e "Cleaning local build environment."
+	echo -e ""
 	docker stop ${REGISTRY_APP_NAME} 2>/dev/null; true
 	docker rm ${REGISTRY_APP_NAME}  2>/dev/null; true
-	echo -n ""
-	echo -n "Purging local images."
+	echo -e ""
+	echo -e "Purging local images."
 	docker rmi ${REGISTRY_DOCKER_REPO} 2>/dev/null; true
 }
 
 help() {
-	echo -n ""
-	echo -n "Please use \`make <target>' where <target> is one of"
-	echo -n ""
-	echo -n "  build		Builds the docker image."
-	echo -n "  test		Tests image."
-	echo -n "  release	Releases images."
-	echo -n "  clean		Cleans local images."
-	echo -n ""
-	echo -n ""
+	echo -e "USAGE\n"
+	echo -e "Please use \`$0 <target>' where <target> is one of\n"
+	echo -e ""
+	echo -e "  build		Builds the docker image.\n"
+	echo -e "  test		Tests image.\n"
+	echo -e "  release	Releases images.\n"
+	echo -e "  clean		Cleans local images.\n"
+	echo -e ""
+	echo -e ""
 }
 
-[[ "$1" == "build" ]] && build
-[[ "$1" == "test" ]] && test
-[[ "$1" == "release" ]] && release
-[[ "$1" == "clean" ]] && clean
-[[ "$1" == "help" ]] && help
-exit 0
+if [[ "$1" == "build" ]]; then build;
+elif [[ "$1" == "test" ]]; then test;
+elif [[ "$1" == "release" ]];then release;
+elif [[ "$1" == "clean" ]]; then clean;
+elif [[ "$1" == "help" ]];then help;
+else
+	help
+	exit -1
+fi
